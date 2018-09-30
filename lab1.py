d7b9514ebd1eb7b378cca2779a659755679ec744
andrew <soutvoid@gmail.com>
2018-09-30 13:05:11 +0300

    expectancy added

--- lab1.py
+++ lab1.py
@@ -1,6 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats as stat
+import numpy as np
+
 plt.style.use('bmh')
 
 K = 1000  # число реализаций Гауссовской случайной величины (СВ)
@@ -19,13 +19,25 @@ x = mu + sig * (np.sum(alf, 0) - 6)  # сумма по столбцам матр
 ## 2. Вычисление выборочных дисперсий
 ds = np.zeros(K)
 for k in range(1, K):
-    ds[k] = np.var(x[0: k])  # среднее первых k реализаций
+    ds[k] = np.var(x[0: k])
 
-plt.plot(np.arange(0, K), ds)  # отображение зависимости выборочных средних от числа реализаций СВ
-plt.plot(np.arange(0, K), d * np.ones( K))  # отображение значения мат. ожидания
+plt.plot(np.arange(0, K), ds)
+plt.plot(np.arange(0, K), d * np.ones(K))
+plt.title("Дисперсия")
 plt.show()
 
-## 3. Плотность и гистограмма
+## 3. Вычисление выборочных матожиданий
+ms = np.zeros(K)
+for k in range(1, K):
+    ms[k] = np.average(x[0: k])
+
+plt.figure()
+plt.plot(np.arange(0, K), ms)
+plt.plot(np.arange(0, K), m * np.ones(K))
+plt.title("Матожидание")
+plt.show()
+
+## 4. Плотность и гистограмма
 t = np.arange(np.min(x), np.max(x), 0.05)     # упорядоченный массив возможных значений случайной величины
 px = 1/(sig*np.sqrt(2*np.pi))*np.exp(-0.5*(t-mu)**2/sig**2)  # формула плотности из таблицы
 
@@ -33,4 +45,5 @@ px = 1/(sig*np.sqrt(2*np.pi))*np.exp(-0.5*(t-mu)**2/sig**2)  # формула п
 plt.figure()
 plt.hist(x,t, density=True, histtype='bar')
 plt.plot(t, px, 'g')   # отрисовка графика плотности
+plt.title("Плотность и возможные значение")
 plt.show()
